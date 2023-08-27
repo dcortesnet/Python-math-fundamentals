44db7ddd00dc5052c43775e4345c536aa05bee8c
dcortesnet <dcortes.net@gmail.com>
2023-08-27 19:25:31 -0400

    Complement and difference

---
+++ 03 sets/complement/complement.py
@@ -0,0 +1,6 @@
+u = { 1,2,3,4,5,6 } 
+a = { 2,4,6 }
+
+complement = u - a
+
+print(complement) # {1, 3, 5}
---
+++ 03 sets/difference/difference.py
@@ -0,0 +1,8 @@
+a = {1,2,3,4,5}
+b = {3,4,5}
+
+differenceAB = a.difference(b)
+differenceBA = b.difference(a)
+
+print(differenceAB) # {1, 2}
+print(differenceBA) # {}
