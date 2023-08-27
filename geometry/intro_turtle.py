249d385ff5c7ad855d8dd160b47dd799d0581446
dcortesnet <dcortes.net@gmail.com>
2023-08-27 12:45:32 -0400

    Intro turtle code

---
+++ geometry/intro_turtle.py
@@ -0,0 +1,10 @@
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("white")
+screen.setup(width=100, height=100, startx=0, starty=0)
+# position start for draw
+
+turtle.forward(100)  # Next 100 
+turtle.done()
+turtle.mainloop()
